209c3705f2e61c235760883467b7d2047bcf4339
Dreunlimited <dre4311@gmail.com>
2016-01-09 23:14:13 -0500

    4th Commit
    Updated collection view

---
+++ pokedex-by-dre/PokeCell.swift
@@ -0,0 +1,30 @@
+//
+//  PokeCell.swift
+//  pokedex-by-dre
+//
+//  Created by D'Andre Ealy on 1/9/16.
+//  Copyright © 2016 D'Andre Ealy. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+
+    @IBOutlet weak var thumbImg: UIImageView!
+    @IBOutlet weak var nameLbl: UILabel!
+    
+    var pokemon: Pokemon!
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        layer.cornerRadius = 5.0
+    }
+    
+    func configureCell(pokemon: Pokemon){
+        self.pokemon = pokemon
+        
+        nameLbl.text = self.pokemon.name.capitalizedString
+        thumbImg.image = UIImage(named: "\(self.pokemon.pokedexId)")
+    }
+}
