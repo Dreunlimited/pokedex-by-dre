39338cb835b3103123e6c65ee24e8de43f0f7f0a
Dreunlimited <dre4311@gmail.com>
2016-01-09 19:58:23 -0500

    Initial Commit

---
+++ pokedex-by-dre/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pokedex-by-dre
+//
+//  Created by D'Andre Ealy on 1/9/16.
+//  Copyright © 2016 D'Andre Ealy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
